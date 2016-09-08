50884ab846cdd22178f702dcbc120eae5b2d07c1
Suresh Kumar Gadi <gadisureshkumar@gmail.com>
2016-09-07 20:24:13 -0500

    5A answers updated.

--- 2016-07-12/FIME-5A/Homework002/db/calculations.m
+++ 2016-07-12/FIME-5A/Homework002/db/calculations.m
@@ -73,87 +73,42 @@ for itr = 1:size(No1,1)
     fprintf(AnsFile,',%s',num2str(ComplexToIntantVal(Q1IXL, Q1Omega, VarOOF1(itr)/100)));
     fprintf(AnsFile,',%s',num2str(ComplexToIntantVal(Q1IXC, Q1Omega, VarOOF1(itr)/100)));
     
-    
-    
 %     Second question
-    Q2R = VarOOG1(itr)*VarOOH1(itr);
-    fprintf(AnsFile,',%s',num2str(Q2R*(5/6)));
-%     Fourth question
-%     Figure 3
-    Q4Omega = 2*pi*60;
-    Q4R = VarOOH1(itr)*10;
-    Q4XL = 1i*Q4Omega*VarOOI1(itr)/10;
-    Q4XC = 1/(1i*Q4Omega*VarOOJ1(itr)*1e-5);
-    Q4V = VarOOK1(itr)*10;
-    Q4I = Q4V/(Q4R + Q4XL + Q4XC);
-    Q4V_R = Q4I*Q4R;
-    Q4V_XL = Q4I*Q4XL;
-    Q4V_XC = Q4I*Q4XC;
-    Q4P_R = abs(Q4V_R*Q4I);
-    Q4P_XL = abs(Q4V_XL*Q4I);
-    Q4P_XC = -abs(Q4V_XC*Q4I);    
-%     Q4P_R = Q4I*Q4V_R;
-%     Q4P_XL = Q4I*Q4V_XL;
-%     Q4P_XC = Q4I*Q4V_XC;
-    fprintf(AnsFile,',%s',num2str(Q4I));
-    fprintf(AnsFile,',%s',num2str(Q4V_R));
-    fprintf(AnsFile,',%s',num2str(Q4V_XL));
-    fprintf(AnsFile,',%s',num2str(Q4V_XC));
-    fprintf(AnsFile,',%s',num2str(Q4P_R));
-    fprintf(AnsFile,',%s',num2str(Q4P_XL));
-    fprintf(AnsFile,',%s',num2str(Q4P_XC));
-%     Figure 4
-    Q4Z1 = 1/(1/Q4R + 1/Q4XL + 1/Q4XC);
-    Q4Z = Q4R + Q4Z1;
-    Q4I = Q4V/Q4Z;
-    Q4V_R1 = Q4I*Q4R;
-    Q4V_R2 = Q4I*Q4Z1;
-    Q4I_R2 = Q4V_R2/Q4R;
-    Q4I_XL = Q4V_R2/Q4XL;
-    Q4I_XC = Q4V_R2/Q4XC;
-    Q4P_R1 = abs(Q4I*Q4V_R1);
-    Q4P_R2 = abs(Q4I_R2*Q4V_R2);
-    Q4P_XL = abs(Q4I_XL*Q4V_R2);
-    Q4P_XC = -abs(Q4I_XC*Q4V_R2);
-    fprintf(AnsFile,',%s',num2str(Q4V_R1));
-    fprintf(AnsFile,',%s',num2str(Q4V_R2));
-    fprintf(AnsFile,',%s',num2str(Q4I));
-    fprintf(AnsFile,',%s',num2str(Q4I_R2));
-    fprintf(AnsFile,',%s',num2str(Q4I_XC));
-    fprintf(AnsFile,',%s',num2str(Q4I_XL));
-    fprintf(AnsFile,',%s',num2str(Q4P_R1));
-    fprintf(AnsFile,',%s',num2str(Q4P_R2));
-    fprintf(AnsFile,',%s',num2str(Q4P_XL));
-    fprintf(AnsFile,',%s',num2str(Q4P_XC));
+%     Sine wave
+    fprintf(AnsFile,',%s',num2str(VarOOG1(itr)/sqrt(2)));
+    fprintf(AnsFile,',%s',num2str(VarOOG1(itr)*2/pi));
+    fprintf(AnsFile,',%s',num2str(sqrt(2)));
+    fprintf(AnsFile,',%s',num2str(pi/(2*sqrt(2))));
+%     Half-wave rectified sine
+    fprintf(AnsFile,',%s',num2str(VarOOI1(itr)/2));
+    fprintf(AnsFile,',%s',num2str(VarOOI1(itr)/pi));
+    fprintf(AnsFile,',%s',num2str(2));
+    fprintf(AnsFile,',%s',num2str(pi/2));
+%     Full-wave rectified sine
+    fprintf(AnsFile,',%s',num2str(VarOOK1(itr)/sqrt(2)));
+    fprintf(AnsFile,',%s',num2str(VarOOK1(itr)*2/pi));
+    fprintf(AnsFile,',%s',num2str(sqrt(2)));
+    fprintf(AnsFile,',%s',num2str(pi/(2*sqrt(2))));
+%     Triangle wave
+    fprintf(AnsFile,',%s',num2str(VarOOM1(itr)/sqrt(3)));
+    fprintf(AnsFile,',%s',num2str(VarOOM1(itr)/2));
+    fprintf(AnsFile,',%s',num2str(sqrt(3)));
+    fprintf(AnsFile,',%s',num2str(2/sqrt(3)));
+%     Sawtooth wave
+    fprintf(AnsFile,',%s',num2str(VarOOO1(itr)/sqrt(3)));
+    fprintf(AnsFile,',%s',num2str(VarOOO1(itr)/2));
+    fprintf(AnsFile,',%s',num2str(sqrt(3)));
+    fprintf(AnsFile,',%s',num2str(2/sqrt(3)));
+%     Rectangular wave
+    fprintf(AnsFile,',%s',num2str(VarOOQ1(itr)));
+%     Pulse wave
+    Q2DutyC = VarOOT1(itr)/10;
+    fprintf(AnsFile,',%s',num2str(VarOOS1(itr)*sqrt(Q2DutyC)));
+    fprintf(AnsFile,',%s',num2str(VarOOS1(itr)*Q2DutyC));
+    fprintf(AnsFile,',%s',num2str(1/sqrt(Q2DutyC)));
     
-
-%     Q1A = Qtns(itr,1) + 1i*Qtns(itr,2);
-%     Q1B = Qtns(itr,3) + 1i*Qtns(itr,4);
-%     Q1C = Qtns(itr,5) + 1i*Qtns(itr,6);
-%     Q1AnsA = Q1A^3;
-%     Q1AnsB = Q1A^2*Q1B/(Q1C);
-%     Q1AnsC = Q1A/Q1B + Q1C;
-%     Q1AnsD = Q1A/(Q1B/Q1C+Qtns(itr,2));
-%     
-%     Ans(itr,1) = real(Q1AnsA);
-%     Ans(itr,2) = imag(Q1AnsA);
-%     Ans(itr,3) = real(Q1AnsB);
-%     Ans(itr,4) = imag(Q1AnsB);
-%     Ans(itr,5) = real(Q1AnsC);
-%     Ans(itr,6) = imag(Q1AnsC);
-%     Ans(itr,7) = real(Q1AnsD);
-%     Ans(itr,8) = imag(Q1AnsD);
-%     
-%     Q2R = Qtns(itr,7)*Qtns(itr,8);
-%     Ans(itr,9) = Q2R*(5/6);
-%     
-%     Q4Omega = 2*pi*60;
-%     Q4R = Qtns(itr,8)*10;
-%     Q4XL = 1i*Q4Omega*Qtns(itr,9)/10;
-%     Q4XC = 1/(1i*Q4Omega*Qtns(itr,10)*1e-6);
-%     Q4V = Qtns(itr,10)*10;
-%     Ans(itr,8) = (Q1AnsD);
-    for itr2 = 1:(52-21)
+%     Finishing the CSV line
+    for itr2 = 1:(52-45)
         fprintf(AnsFile,',');
     end
     fprintf(AnsFile,'\n');
