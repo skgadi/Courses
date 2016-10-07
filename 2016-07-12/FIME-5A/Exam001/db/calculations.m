851de08a8c402cc90efaed10cdef02136f0433ed
Suresh Kumar Gadi <gadisureshkumar@gmail.com>
2016-10-07 10:58:53 -0500

    Exam 1 of 5A is updated.

---
+++ 2016-07-12/FIME-5A/Exam001/db/calculations.m
@@ -0,0 +1,41 @@
+clc;
+clear;
+% copyfile('questions.csv','answers.csv')
+% Qtns = csvread('questions.csv',1,2);
+[No1,Name1,VarOOA1,VarOOB1,VarOOC1,VarOOD1,VarOOE1,VarOOF1,VarOOG1,VarOOH1,VarOOI1,VarOOJ1,...
+    VarOOK1,VarOOL1,VarOOM1,VarOON1,VarOOO1,VarOOP1,VarOOQ1,VarOOR1,VarOOS1,VarOOT1,VarOOU1,...
+    VarOOV1,VarOOW1,VarOOX1,VarOOY1,VarOOZ1,VarOOa1,VarOOb1,VarOOc1,VarOOd1,VarOOe1,VarOOf1,...
+    VarOOg1,VarOOh1,VarOOi1,VarOOj1,VarOOk1,VarOOl1,VarOOm1,VarOOn1,VarOOo1,VarOOp1,VarOOq1,...
+    VarOOr1,VarOOs1,VarOOt1,VarOOu1,VarOOv1,VarOOw1,VarOOx1,VarOOy1,VarOOz1]...
+    = ImportQuestions ('questions.csv', 2);
+Alphabet = char(('A':'Z').',('a':'z').');
+[I,J] = meshgrid(1:52,1:52);
+OutVar=[Alphabet(I(:)), Alphabet(J(:))];
+OutVar=char(Alphabet,OutVar);
+NoOfOutVar = 52;
+% Ans = zeros(size(Qtns));
+AnsFile=fopen( 'answeres.csv', 'wt' );%fopen('answeres.csv');
+fprintf(AnsFile,'No,Name');
+for itr = 1:(NoOfOutVar)
+    fprintf(AnsFile,',SKGVar%s',OutVar(itr));
+end
+fprintf(AnsFile,'\n');
+
+for itr = 1:size(No1,1)
+%     Number and Name
+    fprintf(AnsFile,'%s,%s',char(No1(itr)),char(Name1(itr)));
+%     First question
+    Q1Z1 = VarOOA1(itr) + VarOOB1(itr)*i;
+    Q1Z2 = VarOOC1(itr) + VarOOD1(itr)*i;
+    Q1Z = 3*Q1Z1+Q1Z2;
+    Q1Zeq = 1/(1/Q1Z+1/(2*Q1Z));
+    fprintf(AnsFile,',%s',num2str(Q1Zeq));
+    
+%     Finishing the CSV line
+    for itr2 = 1:(52-1)
+        fprintf(AnsFile,',');
+    end
+    fprintf(AnsFile,'\n');
+end
+
+fclose(AnsFile);
